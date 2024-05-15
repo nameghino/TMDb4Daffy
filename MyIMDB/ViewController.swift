27932ed83dab60430cee7eb78dc59fd6790364cb
Nico Ameghino <nicolas.ameghino@gmail.com>
2024-05-15 11:02:00 -0300

    Initial Commit

---
+++ MyIMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyIMDB
+//
+//  Created by Nicolas Ameghino on 15/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

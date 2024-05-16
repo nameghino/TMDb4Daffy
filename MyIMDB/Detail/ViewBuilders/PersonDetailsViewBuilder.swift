11d782982889f0f1dab6ae497bfdebea7f1cbfc5
Nico Ameghino <nicolas.ameghino@gmail.com>
2024-05-16 11:04:03 -0300

    Implement Person details

--- MyIMDB/Detail/ViewBuilders/PersonDetailsViewBuilder.swift
+++ MyIMDB/Detail/ViewBuilders/PersonDetailsViewBuilder.swift
@@ -16,14 +16,144 @@ class PersonDetailsViewBuilder: DetailsViewBuilder {
         self.viewModel = viewModel
     }
 
-    func buildCard() -> UIView {
+    private lazy var container: UIView = {
+        return UIView()
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 8
+
+        stackView.addArrangedSubview(self.nameLabel)
+
+        if let dobLabel {
+            stackView.addArrangedSubview(dobLabel)
+        }
+
+        if let profileImageView {
+            stackView.addArrangedSubview(profileImageView)
+        }
+
+        if let creditsStackView {
+            stackView.addArrangedSubview(creditsStackView)
+        }
+
+
+        return stackView
+    }()
+
+    private lazy var nameLabel: UIView = {
         let label = UILabel()
-        label.text = viewModel.person.name
-        label.textColor = .red
-        label.backgroundColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = viewModel.name
+        label.numberOfLines = 0
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.textColor = .black
+        label.backgroundColor = .clear
         label.sizeToFit()
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
         return label
+    }()
+
+    private lazy var dobLabel: UIView? = {
+        guard let dob = viewModel.dateOfBirthString else { return nil }
+
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = dob
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        label.textColor = .black
+        label.backgroundColor = .clear
+        label.sizeToFit()
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return label
+    }()
+
+    private lazy var profileImageView: UIImageView? = {
+        guard let posterPath = viewModel.profilePath else { return nil }
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.alpha = 0
+
+        Task {
+            do {
+                let image: UIImage = try await self.getImage(for: posterPath, type: .poster)
+                let aspectRatio = image.size.width / image.size.height
+
+                await NSLayoutConstraint.activate([
+                    imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: aspectRatio)
+                ])
+
+                DispatchQueue.main.async {
+                    imageView.image = image
+                    UIView.animate(withDuration: 0.3) {
+                        imageView.alpha = 1
+                    }
+                }
+            }
+        }
+
+        return imageView
+    }()
+
+    private lazy var creditsStackView: UIStackView? = {
+        let stackView = UIStackView()
+        stackView.spacing = 3
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+
+        Task {
+            self.viewModel.onCreditsUpdated = {
+                DispatchQueue.main.async {
+                    stackView.arrangedSubviews.forEach { stackView.removeArrangedSubview($0) }
+                    stackView.addArrangedSubview(self.creditsHeaderLabel)
+                    for (iconName, credit) in self.viewModel.knownFor {
+                        let row = self.buildCreditsRow(iconName: iconName, credit: credit)
+                        stackView.addArrangedSubview(row)
+                    }
+                }
+            }
+
+            self.viewModel.loadCredits()
+        }
+
+        return stackView
+    }()
+
+    private lazy var creditsHeaderLabel: UIView = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Known for"
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.textColor = .black
+        label.backgroundColor = .clear
+        label.sizeToFit()
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return label
+    }()
+
+    func buildCreditsRow(iconName: String, credit: String) -> UIView {
+        let row = UIStackView()
+        row.axis = .horizontal
+        row.spacing = 5
+
+        let iconView = UIImageView(image: UIImage(systemName: iconName))
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        iconView.widthAnchor.constraint(equalTo: iconView.heightAnchor).isActive = true
+        row.addArrangedSubview(iconView)
+        
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = credit
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        row.addArrangedSubview(label)
+
+        return row
     }
 
-    var images: [ImageKey : Data] = [:]
+    func buildCard() -> UIView {
+        self.stackView
+    }
 }
--- MyIMDB/Detail/ViewModels/PersonDetailsViewModel.swift
+++ MyIMDB/Detail/ViewModels/PersonDetailsViewModel.swift
@@ -14,4 +14,71 @@ class PersonDetailsViewModel {
     init(person: Person) {
         self.person = person
     }
+
+    var onCreditsUpdated: (() -> Void)?
+
+    var name: String { self.person.name }
+
+    private static var dobFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.locale = Locale.autoupdatingCurrent
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+
+    private static var yearFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "y"
+        return df
+    }()
+
+    var dateOfBirthString: String? {
+        return self.person.birthday.map { PersonDetailsViewModel.dobFormatter.string(from: $0) }
+    }
+
+    var profilePath: URL? { return person.profilePath }
+
+    func loadCredits() {
+        Task {
+            let client = TMDBClient()
+            let results = try await client.credits(for: self.person)
+            var shows: [(symbolName: String, show: String)] = []
+
+            for result in results.allShows.prefix(10) {
+                switch result {
+                case .movie(let movie):
+                    var s = movie.title
+                    if let releaseDate = movie.releaseDate {
+                        s += " (\(PersonDetailsViewModel.yearFormatter.string(from: releaseDate)))"
+                    }
+                    shows.append(("movieclapper", s))
+                case .tvSeries(let series):
+                    var s = series.name
+
+                    var periodString = ""
+                    if let fad = series.firstAirDate {
+                        periodString += "\(PersonDetailsViewModel.yearFormatter.string(from: fad)) - "
+                    }
+
+                    if let lad = series.lastAirDate {
+                        periodString += "\(PersonDetailsViewModel.yearFormatter.string(from: lad))"
+                    }
+
+                    if !periodString.isEmpty {
+                        s += " (\(periodString))"
+                    }
+
+                    shows.append(("tv", s))
+                }
+            }
+
+            self.knownFor = shows
+            DispatchQueue.main.async { self.onCreditsUpdated?() }
+        }
+    }
+
+    var knownFor: [(symbolName: String, show: String)] = []
+
+
 }
